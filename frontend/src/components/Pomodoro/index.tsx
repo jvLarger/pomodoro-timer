a4d9c3c11f0f0aadcc390abb2728b56207cf42cb
João Victor <larger13@hotmail.com>
2022-07-23 10:14:11 -0300

    knowing timer solutions

--- frontend/src/components/Pomodoro/index.tsx
+++ frontend/src/components/Pomodoro/index.tsx
@@ -1,9 +1,48 @@
+import { SetStateAction, useEffect, useRef, useState } from 'react';
+import Timer from '../Timer';
 import style from './styles.module.css';
 
 function Pomodoro() {
+
+    const [nrSegundosRestantes, setNrSegundosRestantes] = useState(20);
+    const [isStarted, setIsStarted] = useState(false);
+    const timerRef = useRef<number>();
+
+    useEffect(() => {
+        isStarted ? startTimer() : pauseTimer();
+    }, [isStarted]);
+
+    useEffect(() => {
+
+        console.log(nrSegundosRestantes);
+
+        if (nrSegundosRestantes == 0) {
+            pauseTimer();
+            setNrSegundosRestantes(20);
+        }
+
+    }, [nrSegundosRestantes]);
+
+    function handleStart() {
+        setIsStarted(!isStarted);
+    }
+    
+    function showTimer() {
+        setNrSegundosRestantes(oldCount => (oldCount - 1));
+      }
+
+    function startTimer() {
+        timerRef.current = setInterval(showTimer, 1000);
+    }
+    
+    function pauseTimer() {
+        clearInterval(timerRef.current);
+    }
+
     return (
         <>
-            <h2>Pomodoro</h2>
+            <button onClick={() => { handleStart() }} type='button'>{isStarted ? "Pause" : "Start"}</button>
+            <Timer />
         </>
     );
 }
---
+++ frontend/src/components/Timer/index.tsx
@@ -0,0 +1,10 @@
+function Timer() {
+    return (
+        <>
+            <h2>25:00</h2>
+             
+        </>
+    ); 
+}
+
+export default Timer;
